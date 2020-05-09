4508e01d972d8820649dbfba6b557d9479a58b30
luoyunchong <luoyunchong@foxmail.com>
2020-05-09 19:19:10 +0800

    fix .net core3.1 升级问题

--- aspnetcore-identityserver4/ApiService/Startup.cs
+++ aspnetcore-identityserver4/ApiService/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -15,14 +16,9 @@ namespace ApiService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment  env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -30,10 +26,7 @@ namespace ApiService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-
             services.AddControllers();
-            
 
             // IdentityServer
             services.AddAuthentication(Configuration["Identity:Scheme"])
@@ -44,33 +37,41 @@ namespace ApiService
                     options.ApiName = Configuration["Service:Name"]; // match with configuration in IdentityServer
                 });
 
-            // Swagger
+            #region Swagger
+
+            //Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                options.SwaggerDoc("v1", new OpenApiInfo() { Title = "ApiService", Version = "v1" });
+                var security = new OpenApiSecurityRequirement()
                 {
-                    Version = "v1",
-                    Title = " API 文档",
-                    Description = "by bj "
-                });
-
-                options.IncludeXmlComments(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ApiService.xml"));
-
-                var security = new Dictionary<string, IEnumerable<string>> { { "Bearer", new string[] { } }, };
+                    { new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    }, Array.Empty<string>() }
+                };
                 options.AddSecurityRequirement(security);//添加一个必须的全局安全信息，和AddSecurityDefinition方法指定的方案名称要一致，这里是Bearer。
-                options.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Description = "JWT授权(数据将在请求头中进行传输) 参数结构: \"Authorization: Bearer {token}\"",
                     Name = "Authorization",//jwt默认的参数名称
-                    In = "header",//jwt默认存放Authorization信息的位置(请求头中)
-                    Type = "apiKey"
+                    In = ParameterLocation.Header,//jwt默认存放Authorization信息的位置(请求头中)
+                    Type = SecuritySchemeType.ApiKey
                 });
-            });
 
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, $"{typeof(Startup).Assembly.GetName().Name}.xml");
+                options.IncludeXmlComments(xmlPath);
+
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment  env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -81,7 +82,8 @@ namespace ApiService
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            // swagger
+            app.UseHttpsRedirection();
+
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
@@ -90,10 +92,9 @@ namespace ApiService
             });
 
             app.UseAuthentication();
-            app.UseHttpsRedirection();
-            
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
+            app.UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
--- aspnetcore-identityserver4/IdentityServer4/Startup.cs
+++ aspnetcore-identityserver4/IdentityServer4/Startup.cs
@@ -16,7 +16,6 @@ namespace IdentityServer4.Service
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             InMemoryConfiguration.Configuration = this.Configuration;
@@ -26,21 +25,18 @@ namespace IdentityServer4.Service
                 .AddTestUsers(InMemoryConfiguration.GetUsers().ToList())
                 .AddInMemoryClients(InMemoryConfiguration.GetClients())
                 .AddInMemoryApiResources(InMemoryConfiguration.GetApiResources());
-            // for QuickStart-UI
             services.AddControllersWithViews();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment  env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
 
             app.UseIdentityServer();
-            app.UseStaticFiles();
-            
+
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
